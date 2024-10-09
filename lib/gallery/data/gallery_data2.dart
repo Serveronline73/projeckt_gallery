6a0527f33e8410a4cfb3356d25a9245932a4b3d0
dennis <info.dennis.durmus@gmail.com>
2024-10-09 18:38:24 +0200

    Gallery Data 2 Class für eine weitere Screen

---
+++ lib/gallery/data/gallery_data2.dart
@@ -0,0 +1,30 @@
+// für eine weitere Gallery erstellung Bilder und Text
+
+class GalleryData2 {
+  static const imagePaths = [
+    "asset/car1.jpeg",
+    "asset/car2.jpeg",
+    "asset/car3.jpeg",
+    "asset/car4.jpeg",
+    "asset/car5.jpeg",
+    "asset/car6.jpeg",
+  ];
+
+  static final List<String> imageCaptions = [
+    "Handwäsche Pur!",
+    "Achtung Spiegel Glatt",
+    "Versieglung Pur",
+    "Politur",
+    "Entstauben Pur",
+    "Innen Reinigung",
+  ];
+
+  static final List<String> detailTexts = [
+    "Handwäsche pur, Angebot bis: 31.12.2024",
+    "Achtung! Spiegel glatt, Angebot bis: 15.01.2025",
+    "Versieglung pur, Angebot bis: 28.02.2025",
+    "Politur, Angebot bis: 30.03.2025",
+    "Entstauben pur, Angebot bis: 15.04.2025",
+    "Innen Reinigung, Angebot bis: 01.05.2025",
+  ];
+}
